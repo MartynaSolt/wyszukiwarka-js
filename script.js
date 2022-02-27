75dd7af3e93588cb6008e30007b23e4e1c60d62b
MartynaSolt <83539177+MartynaSolt@users.noreply.github.com>
2022-02-27 15:33:22 +0100

    js with RegExp

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const search = document.querySelector('.search');
 const li = document.querySelectorAll('li');
 
-const searchEngine = e => {
+// const searchEngine = e => {
 
-    const text = e.target.value.toLowerCase();
+//     const text = e.target.value.toLowerCase();
 
-    li.forEach(el => {
-        if (el.textContent.toLowerCase().indexOf(text) !== -1) {
-            el.style.display = 'block'
+//     li.forEach(el => {
+//         if (el.textContent.toLowerCase().indexOf(text) !== -1) {
+//             el.style.display = 'block'
+//         } else {
+//             el.style.display = 'none'
+//         }
+//     })
+// }
+
+// search.addEventListener('keyup', searchEngine)
+
+// ALTERNATYWNA WERSJA Z RegExp
+
+const searchEngine = () => {
+    li.forEach(item => {
+        const match = new RegExp(search.value, 'i').test(item.textContent)
+        // flaga 'i' mówi, że ignorujemy, wielkość znaku
+
+        if (!match) {
+            item.style.display = 'none'
         } else {
-            el.style.display = 'none'
+            item.style.display = 'block'
         }
     })
 }
